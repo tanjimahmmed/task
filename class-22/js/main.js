4d85980f62b1797631d1db7c224108357b8b0f23
tanjimahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-09-20 20:22:26 +0600

    Add files via upload

---
+++ class-22/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     var s = skrollr.init({
+         render:function(data){
+             
+         }
+     });
+     
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
