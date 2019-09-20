ef19d40ca898916297c1dc1327c5e0779f70f9f9
tanjimahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-09-20 15:39:23 +0600

    Add files via upload

---
+++ class-9/class 9/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        $(".client_testimonial_cruosel").owlCarousel({
+            items: 3,
+            margin: 30,
+            autoplay: true,
+            loop: true,
+            nav: false,
+            dots: true,
+            
+        });
+    });
+
+
+
+}(jQuery));	
