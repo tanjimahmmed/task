8adb009607ef000482fc8aade359ca9e49131467
tanjimahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-09-20 18:17:16 +0600

    Add files via upload

---
+++ class-12/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     $(".homepage_slides").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        dots: true,
+        nav:false,
+    });
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
