2b655edaf6d49202335200125af2381d67029f86
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-09-30 23:26:30 +0600

    Add files via upload

---
+++ class-42/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     $(".large_image").magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true,
+        },
+    });
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
