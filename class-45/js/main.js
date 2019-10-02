0a77c7bf8d3880bedef0f3154e7403cb232cf4fc
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-02 11:27:00 +0600

    Add files via upload

---
+++ class-45/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.staff_slider_list').slick({
+        slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+    });
+  });
+
+
+      
