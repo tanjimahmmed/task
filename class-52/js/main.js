bde3f8be7482fb56d9e4ac513baede53cb4ff295
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-12 18:43:38 +0600

    Add files via upload

---
+++ class-52/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     $('.map')
+     .gmap3({
+       center:[42.3726399, -71.1096528],
+       zoom: 14
+     })
+     .bicyclinglayer();
+     
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
