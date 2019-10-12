c5234b183e65df51c537560b4aec01895d4cf77e
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-12 18:35:00 +0600

    Add files via upload

---
+++ class-48/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     $(".testimonial_author_name").perfectScrollbar({
+            
+    });
+
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
