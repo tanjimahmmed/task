614350d8dec7ccdd7f887a3e525d1af089d96faf
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-15 00:13:18 +0600

    Add files via upload

---
+++ class-58/js/main.js
@@ -0,0 +1,27 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     $(".stick_area").sticky();
+    //  $(window).scroll(function() {
+    //     var scrollTop = $(window).scrollTop(); 
+    //       // display add
+    //       if(scroll >= 100){
+    //           $(".stick_area").addClass("is_stick");
+    //     }
+    //   });
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
