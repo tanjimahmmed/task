638f3138a65272f93d12900d3511a27b21566c8f
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-15 00:23:27 +0600

    Add files via upload

---
+++ class-61/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+    //  $('[data-toggle="tooltip"]').tooltip(); 
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
