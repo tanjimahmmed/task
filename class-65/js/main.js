4b7515392c0b913268733d7b0e6e70a8f1cf91f4
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-16 16:22:57 +0600

    Add files via upload

---
+++ class-65/js/main.js
@@ -0,0 +1,34 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+
+        // -- start work -- //
+        $('#bar1').barfiller({
+            barColor: '#fff',
+        });
+        $('#bar2').barfiller({
+            barColor: '#fff',
+        });
+        $('#bar3').barfiller({
+            barColor: '#fff',
+        });
+        $('#bar4').barfiller({
+            barColor: '#fff',
+        });
+        $('#bar5').barfiller({
+            barColor: '#fff',
+        });
+        $('#bar6').barfiller({
+            barColor: '#fff',
+        });
+
+        // -- End work -- //
+
+    });
+
+    jQuery(window).load(function () {
+
+    });
+
+}(jQuery));	
