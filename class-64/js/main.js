1671ee2b2e7c5c484fe7d2d7114b8c0b475368bf
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-17 19:28:47 +0600

    Add files via upload

--- class-64/js/main.js
+++ class-64/js/main.js
@@ -8,6 +8,10 @@
         $(this).html(event.strftime('<span class="countdown-wrap"><span class="single-countdown-item">%w<br/><span>Weeks</span></span><span class="single-countdown-item">%d<br/><span>Days</span></span><span class="single-countdown-item">%H<br/><span>Hours</span></span><span class="single-countdown-item">%M<br/><span>Minutes</span></span><span class="single-countdown-item">%S<br/><span>Seconds</span> </span></span>'));
       });
 
+      $("#mc-form").ajaxChimp({
+      url:'https://gmail.us20.list-manage.com/subscribe/post?u=47717b27aae0e7f7318cc9c4a&amp;id=a784e83acb'
+      });
+
      // -- End work -- //
             
     });
