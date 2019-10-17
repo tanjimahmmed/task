dd769ff14a225503fdcf3bab030669628a49a61f
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-17 19:30:23 +0600

    Add files via upload

---
+++ class-69/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     $('#mc-form').ajaxChimp({
+         url:'https://gmail.us20.list-manage.com/subscribe/post?u=47717b27aae0e7f7318cc9c4a&amp;id=a784e83acb'
+     });
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
