20ef2d63a176baa2420495962ef8e8fcd3717382
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-20 16:45:13 +0600

    Add files via upload

---
+++ class-74/js/main.js
@@ -0,0 +1,24 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     $('.hover').hover(function(){
+        $(this).addClass('flip');
+    },function(){
+        $(this).removeClass('flip');
+    });
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
