57b820f0225cf4766fa7f195a0c00f0af7ee6616
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-10-24 13:09:59 +0600

    Add files via upload

---
+++ class-82/js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+    $(".homepage_area").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:false,
+        nav:false,
+        dots:true
+    });
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
