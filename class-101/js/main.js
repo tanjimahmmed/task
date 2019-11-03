8fcbb014915875b4aa869f109cdcbebb823a612a
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-11-03 16:57:36 +0600

    Add files via upload

---
+++ class-101/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     $(".portfolio_list").masonry({
+        horizontalOrder: true
+     });
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
