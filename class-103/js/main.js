3cfc75012de0600ff77d1da6dd1d1f7fcc75ca61
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-11-04 13:12:29 +0600

    Add files via upload

---
+++ class-103/js/main.js
@@ -0,0 +1,47 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     $(".portfolio_list").masonry({
+        horizontalOrder: true
+     });
+
+     $(".portfolio_filter li").on('click', function(){
+        var filterData = $(this).attr("data-filter");
+
+      $(".portfolio_list").isotope({
+         filter: filterData,
+      });
+
+      $(".portfolio_filter li").removeClass('active');
+      $(this).addClass('active');
+     });
+
+     $(".portfolio_list").isotope({
+      itemSelector: '.single_portfolio_item',
+      percentPosition: true,
+      masonry: {
+      columnWidth: '.single_portfolio_item',
+      horizontalOrder: true
+      }
+     });
+
+     $("body").perfectScrollbar({
+            
+   });
+
+   //   $("body").perfectScrollbar({
+   //   });
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
