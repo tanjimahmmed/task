5501c52b0084ab4a1b620ce1476b94777cc907f7
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-11-12 13:45:50 +0600

    Add files via upload

---
+++ class-119/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+
+    $(".video_play_btn").modalVideo();
+
+    $(".video_play_btn").on('click', function(){
+        return false;
+    });
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
