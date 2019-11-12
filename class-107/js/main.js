db5981302cf584ed4d0858dffe11a2b36bf47d67
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2019-11-12 13:05:34 +0600

    Add files via upload

---
+++ class-107/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+
+
+
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
