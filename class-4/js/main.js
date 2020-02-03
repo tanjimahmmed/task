2c7bb4d84127ee37f6c498338cbc2507afd11771
Md Tanjim Ahmmed <44721685+tanjimahmmed@users.noreply.github.com>
2020-02-03 15:12:52 +0600

    Add files via upload
    Mistek classes

---
+++ class-4/js/main.js
@@ -0,0 +1,17 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+     // -- start work -- //
+     
+
+     // -- End work -- //
+            
+    });
+
+   jQuery(window).load(function(){
+
+   });
+
+}(jQuery));	
